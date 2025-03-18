e27fede381f27a1dec7682f52f43ba32a5e3568f
Nasif Atique Chowdhury <121179152+nasif03@users.noreply.github.com>
2025-03-18 15:33:41 +0600

    Add user init

---
+++ pages/signup.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import sqlite3
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+def verify_signup(username, password, courses):
+    if username == "" or password == "":
+        st.error("Username or password cannot be empty.")
+        return False
+    
+    if courses == []:
+        st.error("You must select atleast one course.")
+        return False
+
+    cursor.execute(f"SELECT * FROM USERS WHERE username = '{username}'")
+    row = cursor.fetchone()
+
+    if row == None:
+        st.session_state['username'] = username
+        cursor.execute(f"INSERT INTO USERS VALUES('{username}', '{password}')")
+        conn.commit()
+        return True
+    
+    st.error("Username already in use.")
+    return False
+
+st.write("")
+with st.form(key = "Login"):
+    username = st.text_input("Enter your username: ")
+    password = st.text_input("Enter your password: ", type="password")
+    courses = st.multiselect(
+        "What courses would you like to add to your planner?",
+        ["CSE115", "CSE173", "CSE215", "CSE225"],
+    )
+    login_button = st.form_submit_button("Sign Up")
+
+st.page_link("login.py",label="Log In")
+
+if login_button:
+    if verify_signup(username, password, courses):
+        st.switch_page("pages/app.py")
+        
+conn.close()
